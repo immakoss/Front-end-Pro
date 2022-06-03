bab56638018a106028c9f7c528b8738cead08724
Kostiantyn Lutsenko <kossmaster999@gmail.com>
2022-06-03 20:35:52 +0300

    Done!

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 let square = document.querySelector('.square');
 let endPoint = document.documentElement.clientWidth - 100;
+let position = 0;
 
 square.addEventListener('click', function(){
-    let timer = setInterval(function(){
-        for(let i = 0; i <= endPoint; i++){
-            let count = 0;
 
-            if(count === endPoint){
-                clearInterval(timer)
-            } else {
-                square.style.left += 1 +'px'
-                count++
-            }
-        }
-    },50)
+    let movement = setInterval(function() {
+        position++
+        square.style.left = position + 'px'
+        if(position === endPoint) clearInterval(movement)
+
+    },1)
 })
